02b3aea53004536ca2ba4770046ecedb7ff825de
jeongraeJR <hwal92@naver.com>
2020-02-11 14:31:34 +0900

    modify config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps : [
     {
     name: 'task1',
-    script: './task1/index.js',
+    script: './task3/bin/www',
     watch: false,
     max_memory_restart: '200M',
     max_restarts: 5,
   },
   {
     name: 'task2',
-    script: './task2/index.js',
+    script: './task4/bin/www',
     watch: false,
     max_restarts: 5,
     cron_restart:'30 * * * *',
